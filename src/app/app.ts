b2429d4754c3f8893c8a28ae2c7bbe982f6ded30
LevinKrischer <levin@levinkrischer.de>
2026-01-23 17:53:40 +0100

    add: skills section // refactor: renaming image files

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./shared/components/main-sections/header/header";
 import { AboutMe } from "./shared/components/main-sections/about-me/about-me";
+import { Skills } from "./shared/components/main-sections/skills/skills";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, AboutMe],
+  imports: [RouterOutlet, Header, AboutMe, Skills],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
