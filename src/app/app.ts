fad680c87fe013c0bc62cc741abfc8c742f22da9
LevinKrischer <levin@levinkrischer.de>
2026-01-24 19:59:05 +0100

    add: project-section

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from "./shared/components/main-sections/header/header";
 import { AboutMe } from "./shared/components/main-sections/about-me/about-me";
 import { Skills } from "./shared/components/main-sections/skills/skills";
+import { Projects } from './shared/components/main-sections/projects/projects';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, AboutMe, Skills],
+  imports: [RouterOutlet, Header, AboutMe, Skills, Projects],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
