76423b85dd1ed8ac13e47acc8bfe057335e588ee
LevinKrischer <levin@levinkrischer.de>
2026-02-05 10:31:51 +0100

    add: responsive design for skills section // fix: reduce image size off peel-images.

--- src/app/shared/components/elements/peel-off/peel-off.ts
+++ src/app/shared/components/elements/peel-off/peel-off.ts
@@ -10,6 +10,7 @@ export class PeelOff {
 
   @ViewChild('peelClick') peelClick!: ElementRef;
   @ViewChild('peelLayer') peelLayer!: ElementRef;
+  @ViewChild('peelLayerMobile') peelLayerMobile!: ElementRef;
 
   hidePeel() {
     this.peelClick.nativeElement.classList.add('toBeHidden');
@@ -20,6 +21,7 @@ export class PeelOff {
     setTimeout(() => {
       this.peelClick.nativeElement.classList.add('hidden');
       this.peelLayer.nativeElement.classList.add('hidden');
+      this.peelLayerMobile.nativeElement.classList.add('hidden');
     }, 450);
   }
 
